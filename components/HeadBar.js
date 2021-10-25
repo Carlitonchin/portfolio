7252fb0e7f101c070520988020ce06d72d96c974
Carlitonchin <carlozalejandro98@gmail.com>
2021-10-25 21:27:16 +0200

    presentation page

---
+++ components/HeadBar.js
@@ -0,0 +1,22 @@
+import styles from '../styles/HeadBar.module.css'
+import Link from 'next/link'
+
+const HeadBar = function(props)
+{
+
+    console.log(props.links)
+    return (
+    <div className={styles.container}>
+        <div className={styles.containerLogo}>{props.children}</div>
+        <div className={styles.containerOptions}>
+            {props.links.map((value, index)=>
+            {
+              return <Link key={index} href="#"><h3 className={value.selected?styles.selected:""}>{value.text}</h3></Link>
+            })}
+        </div>
+    </div>
+    );
+
+}
+
+export default HeadBar
---
+++ components/Logo.js
@@ -0,0 +1,8 @@
+import styles from '../styles/Logo.module.css'
+
+function Logo()
+{
+    return <div className={styles.container}><p className={styles.p}>{"<CS/>"}</p></div>
+}
+
+export default Logo;
